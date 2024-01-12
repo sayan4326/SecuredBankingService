8cb48e8f7291392bd2582345b93141b151320a62
sayan <sayan4326@gmail.com>
2024-01-12 09:34:48 +0530

    Project Initialzied

---
+++ src/main/java/com/yourcompany/securedbankingservice/SecuredBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yourcompany.securedbankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredBankingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourcompany/securedbankingservice/SecuredBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.securedbankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
