58df975d68055b0e42680c6f7460f2f15694cdfd
Sayan Mallick <sayan4326@gmail.com>
2024-01-18 09:49:57 +0530

    In memory authenication and NoOpPassword encoder implementation

--- src/main/java/com/yourcompany/securedbankingservice/config/SecurityConfig.java
+++ src/main/java/com/yourcompany/securedbankingservice/config/SecurityConfig.java
@@ -1,13 +1,40 @@
 package com.yourcompany.securedbankingservice.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+	@Bean
+	InMemoryUserDetailsManager inMemoryUserDetailsManager()
+	{
+		InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+		List<UserDetails> userDetails = new ArrayList<>();
+		userDetails.add(User.withUsername("Sayan").password("root").authorities("ADMIN").build());
+		userDetails.add(User.withUsername("Rahul").password("system").authorities("CUSTOMER").build());
+		for(UserDetails userDetail : userDetails)
+		{
+			inMemoryUserDetailsManager.createUser(userDetail);
+		}
+		return inMemoryUserDetailsManager;
+	}
+	@Bean
+	PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
 	{
@@ -15,4 +42,5 @@ public class SecurityConfig {
 		httpSecurity.formLogin();
 		return httpSecurity.build();
 	}
+	
 }
